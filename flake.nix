a6eb45a11111c0dcac9109659b75cdc148a3078d
qrxnz <send@qrxnz.dev>
2024-10-29 11:02:26 +0100

    dev flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "dev flake";
+
+  inputs =
+    {
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.x86_64-linux.default =
+        pkgs.mkShell
+          {
+            nativeBuildInputs = with pkgs; [
+              go
+              just
+              watchexec
+            ];
+          };
+    };
+}
