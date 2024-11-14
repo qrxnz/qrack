8afe83d2daaba5dbe51ebeae50bae4629ddd8bc8
qrxnz <send@qrxnz.dev>
2024-11-14 11:49:33 +0100

    example crackme

---
+++ example_crackme/test_crackme.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+#define PASSWORD "arisfertidis"
+
+int main(int argc, char *argv[]) {
+    // Check if the user provided a password as an argument
+    if (argc < 2) {
+        printf("Usage: %s <password>\n", argv[0]);
+        return 1;
+    }
+
+    // Compare the provided password with the correct one
+    if (strcmp(argv[1], PASSWORD) == 0) {
+        printf("Password correct! Access granted.\n");
+    } else {
+        printf("Incorrect password! Access denied.\n");
+    }
+
+    return 0;
+}
