26283c208489b22001e760d83e9649b729fcc1f3
Karol Skoczyk <send@qrxnz.dev>
2026-04-12 19:12:30 +0200

    build: bump version to 2.0.6 and update treefmt in flake.nix

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       in rec {
         packages.default = pkgs.buildGoModule rec {
           pname = "qrack";
-          version = "2.0.5";
+          version = "2.0.6";
           src = self;
 
           vendorHash = "sha256-GqAk9SdbBMGGo6IQp7CMi5LjWf/IFB897vcd4XC867k=";
@@ -28,7 +28,7 @@
             delve
 
             # Formatters
-            treefmt2
+            treefmt
             mdformat
             alejandra
 
